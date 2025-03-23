c605ffa8ad6d90ae90e3c76c571d6e6b3accc411
podolski10 <marouanebenhaddou9@gmail.com>
2025-03-23 17:48:28 +0000

    registred models in admin.py

--- lab_proj/auth_app/admin.py
+++ lab_proj/auth_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Docteur, User, Patient
 # Register your models here.
+
+admin.site.register(Docteur)
+admin.site.register(User)
+admin.site.register(Patient)
--- lab_proj/docteur_app/admin.py
+++ lab_proj/docteur_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Prescription, Message
 # Register your models here.
+admin.site.register(Prescription)
+admin.site.register(Message)
