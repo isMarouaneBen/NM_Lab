1a599100e18f50379464e0c9f84bacc26f5df50d
podolski10 <marouanebenhaddou9@gmail.com>
2025-04-15 04:07:19 +0100

    views commit
    added view to provide data for charts for age repartition , blood groups, and rdv progression by months

--- lab_proj/stat_app/urls.py
+++ lab_proj/stat_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('today-rendezvous-count/',CountTodayRendezVousView, name = 'today-rendezvous-count' ),
     path('moyen-age/', MoyenAgeView , name = 'moyen-age'),
     path('chart/age-partition/', ChartAgeRepartitionView, name='age-partition'),
-    path('chart/rdv-progression', ChartRdvProgressionView, name='rdv-progression'),
-    path('chart/blood-groups', ChartBloodRepartitionView, name='blood-groups')
+    path('chart/rdv-progression/', ChartRdvProgressionView, name='rdv-progression'),
+    path('chart/blood-groups/', ChartBloodRepartitionView, name='blood-groups')
 ]
