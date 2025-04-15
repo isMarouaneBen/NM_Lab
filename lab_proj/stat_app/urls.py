070dbe5e30dc7a86795bf1f94a994702e3fa759a
podolski10 <marouanebenhaddou9@gmail.com>
2025-04-15 04:02:29 +0100

    endpoints commit
    added view to provide data for charts for age repartition , blood groups, and rdv progression by months

--- lab_proj/stat_app/urls.py
+++ lab_proj/stat_app/urls.py
@@ -4,6 +4,8 @@ from .views import *
 urlpatterns = [
     path('monthly-patients/',monthlyPatientView, name = 'monthly-patients'),
     path('today-rendezvous-count/',CountTodayRendezVousView, name = 'today-rendezvous-count' ),
-    path('moyen-age', MoyenAgeView , name = 'moyen-age'),
-
+    path('moyen-age/', MoyenAgeView , name = 'moyen-age'),
+    path('chart/age-partition/', ChartAgeRepartitionView, name='age-partition'),
+    path('chart/rdv-progression', ChartRdvProgressionView, name='rdv-progression'),
+    path('chart/blood-groups', ChartBloodRepartitionView, name='blood-groups')
 ]
